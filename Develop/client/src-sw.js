b0c3701877a2a842cd30260cd557c975e9dcdb2e
Guy Domino <guywayne.domino7@gmail.com>
2023-10-15 17:03:08 -0500

    Finished ToDos in js files

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,21 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // Callback function to determine which requests should be cached
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Define the cache storage name
+    cacheName: 'resources-cache',
+    plugins: [
+      // Plugin to cache responses with specific headers for up to a month
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 4 * 7 * 24 * 60 * 60, // 4 Weeks
+      })
+    ],
+  })
+);
--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+// Connecting to the database and it's version which is 1
+const connect = await openDB('jate', 1);
+// creating a action and giving the database privileges
+const action = connect.transaction('jate', 'readwrite');
+// opening the object store
+const open = action.objectStore('jate');
+// .add() method passes the content
+const info = open.put({ id: 1, value: content});
+// confirms the request.
+const confirm = await request;
+console.log('The data was saved to the db', confirm );
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+// Connecting to the database again
+const connect = await openDB('jate', 1);
+// creating an action and giving it privileges in the db
+const action = connect.transaction('jate', 'readonly');
+// opening the object store
+const open = action.objectStore('jate');
+// use .getAll() to get all of the data
+const info = open.getAll();
+
+const confirm = await request;
+console.log('confirm.value', confirm);
+return confirm.value;
+};
 
 initdb();
